a415c0cdcdd01ee13d40d538e910faf3f3937293
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-01-25 23:22:14 +0900

    Start cache algorithm

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-const app = require('express')()
-const lib = require('./lib')
+const app       = require('express')()
+const request   = require('request')
+const fs        = require('fs')
+const lib       = require('./lib')
+const cashe_dir = __dirname + '/../cache'
 
+// define router
 app.get('/:year/:month/:day', (req, res) => {
 
   if (!lib.validateParams(req.params)) {
@@ -9,14 +13,24 @@ app.get('/:year/:month/:day', (req, res) => {
   } else {
     const { year, month, day } = req.params
 
-    res.write(`<html>
-        ${lib.generateHTMLHead(year, month, day)}
-        ${lib.generateHTMLBody(year, month, day)}
-      </html>`)
-    res.end()
+    fs.createReadStream(`${cashe_dir}/${year}${month}${day}.jpg`)
+      .on('error', () => {
+        // image not found
+        // make request and cache
+      })
+      .on('end', () => {
+        res.write(`<html>
+            ${lib.generateHTMLHead(year, month, day)}
+            ${lib.generateHTMLBody(year, month, day)}
+          </html>`)
+        res.end()
+      })
   }
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('app is listening..')
+// create folder and start listen
+fs.mkdir(cashe_dir, () => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('app is listening..')
+  })
 })
